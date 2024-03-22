1cd7747d27798eb7c28a2c2129269370c7a8ef0c
kimmainsain <kmsid003@gmail.com>
2024-03-22 13:10:38 +0900

    build : axios 설치

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // scss
 import "./AppLayout.scss";
+import axios from "axios";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+
   const value = "value value";
   const value2 = "value2 value2";
-  const [count, setCount] = useState(0);
   const increase = () => {
     setCount(count + 1);
   };
   const decrease = () => {
     setCount(count - 1);
   };
+
+  const getData = async () => {
+    const response = await axios.get(``);
+    const data = await response.json();
+    setData(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    // getData();
+  }, []);
 
   return (
     <div>
@@ -24,6 +38,7 @@ function App() {
       <h2>{count}</h2>
       <button onClick={increase}>+1</button>
       <button onClick={decrease}>-1</button>
+      <h2>HTTP Data : {data}</h2>
     </div>
   );
 }
