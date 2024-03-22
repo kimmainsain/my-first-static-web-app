ff7c8af12e8f81fce43baf3147ee73e19ad66abe
kimmainsain <kmsid003@gmail.com>
2024-03-22 11:18:35 +0900

    Prettier 적용

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   const value = "value value";
   const value2 = "value2 value2";
 
-  return <div>ci 확인 커밋 {value2} {value}</div>;
+  return (
+    <div>
+      ci 확인 커밋 {value2} {value}
+    </div>
+  );
 }
 
 export default App;
